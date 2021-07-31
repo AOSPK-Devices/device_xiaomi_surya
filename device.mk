f9b7110661fceb1878fd3e789b1da8c5bc45fbb8
CHRIS W444 <70589286+ChrisW444@users.noreply.github.com>
2021-07-31 09:11:05 -0400

    surya: update kraken overlays
    Signed-off-by: CHRIS W444 <70589286+ChrisW444@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -309,7 +309,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-kraken
 
 PRODUCT_PACKAGES += \
     KarnaFrameworks \
